1fb045b0b5eb5d325262d469e5fa67b7ee0380d7
Atash <atash.hauschild@web.de>
2023-01-30 14:36:27 +0100

    change msg

--- main.py
+++ main.py
@@ -63,12 +63,8 @@ class Main:
 
     def send_lot_status(self, lot_free, numberplates):
         """Send the new status of the lot to the broker"""
-        msg = {"id": self.id,
-               "timestamp": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-               "lot_free": lot_free,
-               "number_plate": numberplates
-               }
-        self.publisher.publish("status/"+self.id, str(msg), qos=1)
+        msg = '{"id": '+ self.id +', "timestamp": ' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ', "lot_free": ' +lot_free +', "number_plate": ' +numberplates +'}'
+        self.publisher.publish("status/"+self.id, msg, qos=1)
 
     def send_heartbeat(self):
         """Send hearbeat signal to the broker"""
